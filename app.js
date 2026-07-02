0008558205ed4500abccf1ddc17cd6d8aa1d2b28
alaurentrichard-igensia <a.laurentrichard@edu.igensia.com>
2026-07-02 14:00:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 function calcul(a, b) {
   return a + b;
 }
 
-module.exports = calcul;
+app.get('/', (req, res) => {
+  res.send('Hello DevOps - webapp sur Kubernetes ! 2 + 3 = ' + calcul(2, 3));
+});
+
+// Sonde utilisée par Kubernetes
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log('Serveur démarré sur le port ' + PORT);
+  });
+}
+
+module.exports = { calcul, app };
